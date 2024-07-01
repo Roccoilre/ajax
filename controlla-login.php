fe7ae90fc7cbbba27e33e4433e8a0ec027b5c98a
Robby12849 <robydebari2005@gmail.com>
2024-07-01 12:30:33 +0200

    Inizio login

---
+++ controlla-login.php
@@ -0,0 +1,37 @@
+<?php     
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "db_agritrak"; 
+    $conn = new mysqli($servername, $username, $password, $dbname);    
+    if ($conn->connect_error) {
+        echo "Connessione fallita: " . $conn->connect_error;
+    }
+$tab_nome = "utente";  
+$email = strtolower($_POST["email"]);  
+$password = $_POST["password"];   
+$password = stripslashes($password);  
+$email = $conn->real_escape_string($email);  
+$password = $conn->real_escape_string($password);
+$controllo = false;  
+$sql = "SELECT * FROM $tab_nome WHERE email='$email'";  
+$result = $conn->query($sql);  
+$conta = $result->num_rows;     
+if ($conta == 1) {  
+    $row = $result->fetch_assoc(); 
+    $passc = $row['password'];  
+    $id = $row['ID_utente'];
+    $nome = $row['nome'];
+    if (password_verify($password, $passc)) {  
+        $controllo = true;        
+    }     
+}     
+if ($controllo) {         
+    session_start();  
+    $_SESSION['email'] = $email;  
+    $_SESSION['ID_utente'] = $id;
+    $_SESSION['nome'] = $nome;
+        header("Location: 1.html");
+} else {        
+    echo '<script>alert("Credenziali Errate"); window.location = "login.html";</script>';
+}
